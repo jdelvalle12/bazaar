6a4f4bb869a804556ccc52584e34688553576535
Jose Del Valle <jdelvalle88@live.com>
2023-02-16 18:30:01 -0500

    codes added onto product file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -103,13 +103,22 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      product_id: req.params.book_id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
